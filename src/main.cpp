dd014b0520fa3b8448fa43da4e7dee2cf0d87c89
Rafael Niebles <rafaelniebles2003@gmail.com>
2025-07-12 08:14:07 -0400

    add feet rect

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,54 @@ namespace {
 constexpr uint32_t IPHONE_SE_SCREEEN_WIDTH = 750;
 constexpr uint32_t IPHONE_SE_SCREEN_HEIGHT = 1334;
 constexpr uint32_t TARGET_FPS              = 60;
-constexpr int32_t SPRITE_SCALE_FACTOR      = 6;
+constexpr int32_t SPRITE_SCALE_FACTOR      = 5;
 } // namespace
 
 class Niko {
 private:
-  // If there ends up being a lot of assets we might want to move them to a JSON
-  // Also, pass this as a dep if you want to get serious with it
+  /* === Sprite === */
+
+  /**
+   * @note  If there ends up being a lot of assets we might want to move them to a JSON
+   * Also probably wanna pass this as a dep
+   */
   const Texture2D spritesheet =
       LoadTexture(std::filesystem::path("assets/niko.png").c_str());
-  const uint32_t frame_count{8};
-  const uint32_t frame_speed{8}; // Frames per second
-  uint32_t current_frame{0};
-  uint32_t frame_counter{0};
-  Vector2 position{200, 200};
-  Rectangle frame_rect{
-      0, 0, static_cast<float>(spritesheet.width),
-      static_cast<float>(spritesheet.height) /
-          frame_count}; // Only height gets divided by frame count to "cut up"
-                        // the individual frames (they are vertically stacked)
+  const uint32_t frame_count = 8;
+  const uint32_t frame_speed = 8; // Frames per second
+  
+  uint32_t current_frame     = 0;
+  uint32_t frame_counter     = 0;
+  Vector2 position           = {100, 230};
+
+  /**
+   * @note  Only height gets divided by frame count to "cut up"
+   * the individual frames (they are vertically stacked)
+   */
+  Rectangle frame_rect = {0, 0, static_cast<float>(spritesheet.width),
+                          static_cast<float>(spritesheet.height) / frame_count};
+
+  /* === Drawing === */
+
+  const float scaled_width  = SPRITE_SCALE_FACTOR * frame_rect.width;
+  const float scaled_height = SPRITE_SCALE_FACTOR * frame_rect.height;
+
+  Rectangle draw_rect   = {0, 0, 0, 0};
+  Vector2 origin_offset = {scaled_width / 2, scaled_height / 2};
+
+  /* === Jumping === */
+
+  bool is_jumping     = false;
+  bool is_grounded    = false;
+  uint32_t jump_timer = 0;
+  Rectangle feet      = {0, 0, 0, 0};
 
 public:
   Niko() = default;
 
   ~Niko() { UnloadTexture(spritesheet); };
+
+  bool debug = false;
 
   void advanceFrames() {
     frame_counter++;
@@ -48,14 +72,37 @@ public:
     }
   }
 
-  void render(bool debug_overlay) {
+  void move() {
+    /**
+     * @note feet rect is initially identical to draw_rect
+     *
+     * We apply custom width and height and apply some offsets to make them fit the player's sprite better
+     *
+     * These values are entirely eyeballed!
+     */
+
+    const uint32_t feet_width  = 40;
+    const uint32_t feet_height = 10; // How tall the feet rect is
+
+    const int32_t feet_y_offset = -12;
+    const int32_t feet_x_offset = -9;
+
+    feet = {position.x - draw_rect.width / 2 + feet_width + feet_x_offset,
+            position.y + draw_rect.height / 2 - feet_height + feet_y_offset,
+            feet_width, feet_height};
+
+    if (debug) {
+      DrawRectangleLinesEx(feet, 2, YELLOW);
+    }
+  }
+
+  void render() {
     float scaled_width =
         SPRITE_SCALE_FACTOR * static_cast<float>(frame_rect.width);
     float scaled_height =
         SPRITE_SCALE_FACTOR * static_cast<float>(frame_rect.height);
 
-    Rectangle draw_rect{position.x, position.y, scaled_width, scaled_height};
-    Vector2 origin_offset{scaled_width / 2, scaled_height / 2};
+    draw_rect = {position.x, position.y, scaled_width, scaled_height};
 
     /**
      * @note Raylib's only draw fuction that allows scaling AND using a src rect is DrawTexturePro, which is why we need to define raw rect and origin
@@ -70,7 +117,11 @@ public:
 
     DrawTexturePro(spritesheet, frame_rect, draw_rect, origin_offset, 0, WHITE);
 
-    if (debug_overlay) {
+    if (debug) {
+      DrawText("Niko the Nicotine-Addicted\nPunk Salamander",
+               position.x - scaled_width / 2,
+               position.y - scaled_width / 2 - 40, 18, WHITE);
+
       DrawText("POS", position.x - scaled_width / 2,
                position.y + scaled_height / 2, 12, BLUE);
       DrawCircleV(position, 2, BLUE);
@@ -95,6 +146,7 @@ int main(void) {
   SetTargetFPS(60);
 
   Niko niko;
+  niko.debug = true;
 
   while (!WindowShouldClose()) {
     /**
@@ -106,6 +158,7 @@ int main(void) {
     // Game Logic
     // ===================================
 
+    niko.move();
     niko.advanceFrames();
 
     // ===================================
@@ -116,7 +169,7 @@ int main(void) {
 
     ClearBackground(BLACK);
 
-    niko.render(true);
+    niko.render();
 
     EndDrawing();
   }
